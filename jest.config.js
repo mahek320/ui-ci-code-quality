2013d6c63c896fed3617297b731b42bb13bba889
mahek320 <Mahek.10748211@LTIMindtree.com>
2025-06-04 20:42:26 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
+  roots: ["<rootDir>/public/src/components"],
   testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest'
