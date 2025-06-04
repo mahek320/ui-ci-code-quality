61e4128988ef33f1acd39f54e1a16d5fc923065c
mahek320 <Mahek.10748211@LTIMindtree.com>
2025-06-04 21:38:56 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,2 @@
-module.exports = {
-  roots: ["<rootDir>/public/src/components"],
-  testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest'
-  },
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov']
-};
+
 
