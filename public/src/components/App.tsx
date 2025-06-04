222c743073dc3f0a5d91195b7dcc0884c8a15b5c
mahek320 <Mahek.10748211@LTIMindtree.com>
2025-06-04 18:14:32 +0530

    Update App.tsx

--- public/src/components/App.tsx
+++ public/src/components/App.tsx
@@ -1,1 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Hello, React + TypeScript!</h1>
+    </div>
+  );
+};
+
+export default App;
 
