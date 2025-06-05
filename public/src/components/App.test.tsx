1491ee78d5e845d76fbe5772c51d21cfe308fec7
mahek320 <Mahek.10748211@LTIMindtree.com>
2025-06-05 18:24:54 +0530

    Update App.test.tsx

--- public/src/components/App.test.tsx
+++ public/src/components/App.test.tsx
@@ -1,12 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders the heading', () => {
+test('renders heading', () => {
   render(<App />)
   expect(screen.getByText(/Hello, React/i)).toBeInTheDocument()
-})
-
-test('renders the paragraph', () => {
-  render(<App />)
-  expect(screen.getByText(/Missing semicolon/i)).toBeInTheDocument()
 })
