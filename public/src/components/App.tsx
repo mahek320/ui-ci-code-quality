e4b6ed43b2eda4dc9c80476e3bd638ba073f30ef
mahek320 <Mahek.10748211@LTIMindtree.com>
2025-06-05 13:48:34 +0530

    Update App.tsx

--- public/src/components/App.tsx
+++ public/src/components/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from 'react'
 
 const App: React.FC = () => {
+  const unusedVar = 42 // ESLint: unused variable
   return (
     <div className="App">
       <h1>Hello, React + TypeScript!</h1>
+      <p>Missing semicolon</p> {/* Prettier: formatting issue */}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
+
 
