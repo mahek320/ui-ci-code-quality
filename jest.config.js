81d5eb5d43e4fc2cdf4645eb361d05285fd944c9
mahek320 <Mahek.10748211@LTIMindtree.com>
2025-06-05 15:19:29 +0530

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  roots: ['<rootDir>/public/src'],
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+};
