de11076995767d925e05c8fb61a79902738549fe
mahek320 <Mahek.10748211@LTIMindtree.com>
2025-06-05 18:32:21 +0530

    Update App.test.tsx

--- public/src/components/App.test.tsx
+++ public/src/components/App.test.tsx
@@ -5,3 +5,7 @@ test('renders heading', () => {
   render(<App />)
   expect(screen.getByText(/Hello, React/i)).toBeInTheDocument()
 })
+test('renders paragraph', () => {
+  render(<App />)
+  expect(screen.getByText(/sample paragraph/i)).toBeInTheDocument()
+})
