5e390bc96a48409719a59698dd7f520aba0a494d
mahek320 <Mahek.10748211@LTIMindtree.com>
2025-06-05 18:25:17 +0530

    Update index.tsx

--- public/src/components/index.tsx
+++ public/src/components/index.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-console.log('Rendering App')  // Prettier: double space, ESLint: console.log warning
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+)
 root.render(
   <React.StrictMode>
-    <App/>
-  </React.StrictMode> // Prettier: spacing issue
+    <App />
+  </React.StrictMode>
 )
