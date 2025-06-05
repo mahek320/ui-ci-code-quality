c71f88c3ba80057b758d4ac12179e0a3cf608b52
mahek320 <Mahek.10748211@LTIMindtree.com>
2025-06-05 18:18:59 +0530

    Update App.test.tsx

--- public/src/components/App.test.tsx
+++ public/src/components/App.test.tsx
@@ -1,14 +1,12 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders heading', () => {
-  render(<App />);
-  expect(screen.getByText(/Hello, React/i)).toBeInTheDocument();
-});
+test('renders the heading', () => {
+  render(<App />)
+  expect(screen.getByText(/Hello, React/i)).toBeInTheDocument()
+})
 
-test('clicking button increments counter', () => {
-  render(<App />);
-  const button = screen.getByText('Click me');
-  fireEvent.click(button);
-  expect(screen.getByText(/Click count: 1/i)).toBeInTheDocument();
-});
+test('renders the paragraph', () => {
+  render(<App />)
+  expect(screen.getByText(/Missing semicolon/i)).toBeInTheDocument()
+})
