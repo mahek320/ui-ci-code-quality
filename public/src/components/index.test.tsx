925d5870c821148218bf6ff9fe1b31cec6956f66
mahek320 <Mahek.10748211@LTIMindtree.com>
2025-06-05 18:19:17 +0530

    Update index.test.tsx

--- public/src/components/index.test.tsx
+++ public/src/components/index.test.tsx
@@ -2,10 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-test('renders App component without crashing', () => {
+test('renders App without crashing', () => {
   const div = document.createElement('div')
   document.body.appendChild(div)
-
-  const root = ReactDOM.createRoot(div)
-  root.render(<App />)
+  ReactDOM.createRoot(div).render(<App />)
 })
+
