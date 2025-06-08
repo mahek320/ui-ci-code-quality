6c9f133331a292ae40b5a89b9bfd9fc593ab5d27
mahek320 <Mahek.10748211@LTIMindtree.com>
2025-06-08 16:10:33 +0530

    Delete public/src/components/App.test.tsx

--- public/src/components/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen, fireEvent } from '@testing-library/react'
-import App from './App'
-
-// Remove this line: jest.mock('./App')
-
-describe('App Component', () => {
-  // ... rest of your tests remain the same
-})
