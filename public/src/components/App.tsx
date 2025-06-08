0a5582cc7c028e5b62ec88eb5ca1802708d8df86
mahek320 <Mahek.10748211@LTIMindtree.com>
2025-06-08 13:39:51 +0530

    Update App.tsx

--- public/src/components/App.tsx
+++ public/src/components/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import UserCard from './components/UserCard'
 
 const App: React.FC = () => {
   const [count, setCount] = useState(0)
@@ -9,9 +10,14 @@ const App: React.FC = () => {
       <p>This is a sample paragraph</p>
       <p data-testid="count">Clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
+
+      {/* 👇 Add your UserCard component inside the JSX */}
+      <UserCard name="Mahek Shetty" age={22} isActive={true} />
     </div>
   )
 }
 
 export default App
+
+
 
