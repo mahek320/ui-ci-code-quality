528d9036cbb7cc3ac87938fd0e759a091dadaf3c
mahek320 <Mahek.10748211@LTIMindtree.com>
2025-06-08 19:16:51 +0530

    Delete run_retirejs.sh

--- run_retirejs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-npm install -g retire
-retire --path public/src/components --outputformat json --outputpath retire-report.txt
-if grep -q '"data":[]' retire-report.txt; then
-    echo "No vulnerabilities found." > retire-report.txt
-fi
